593d00c965d2167801fa59c736e0b3e100b087d9
cihataltuntas <btabcdef>
2020-03-29 11:56:36 +0300

    chore: added project files

---
+++ src/main/java/com/ericsson/poccontractmanagement/PocContractManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.poccontractmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocContractManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocContractManagementApplication.class, args);
+	}
+
+}
